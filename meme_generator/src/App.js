a71f010e14c7486d9747de489d71f9b14ad595c2
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-01-31 18:52:48 +0100

    added the input and the buttons for my meme generator along with stylings

--- meme_generator/src/App.js
+++ meme_generator/src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/header'
 import './App.css';
+import Meme from './components/meme'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Meme />
     </div>
   );
 }
--- meme_generator/src/components/header.js
+++ meme_generator/src/components/header.js
@@ -1,10 +1,10 @@
 export default function Header(){
     return (
         <div>
-            <header class="main-header">
-                <img class="header-img" src={require("../images/troll-face.png")} alt="troll face"/>
-                <h2 class="header-title">Meme Generator</h2>
-                <p class="header-project">React Course - Project 3</p>
+            <header className="main-header">
+                <img className="header-img" src={require("../images/troll-face.png")} alt="troll face"/>
+                <h2 className="header-title">Meme Generator</h2>
+                <p className="header-project">React Course - Project 3</p>
             </header>
         </div>
     )
