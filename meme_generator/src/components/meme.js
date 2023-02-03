4ea0ca0e780d856756184d192937e9e2f2d7b962
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-02-03 16:38:37 +0100

    added in a onClick that grabs a random url from memesData.js so I can start to add in the random images.

--- meme_generator/src/components/meme.js
+++ meme_generator/src/components/meme.js
@@ -1,11 +1,21 @@
+import memesData from "../memesData"
+
 export default function Meme() {
+    function getMemeImage(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+    }
+    
+    
     return (
         <main className="main">
-            <form className="form">
+            <div className="form">
                 <input type="text" className="form--input" placeholder="Top text"/>
                 <input type="text" className="form--input" placeholder="Bottom text" />
-                <button className="form--btn">Get a new meme image 🖼️</button>
-            </form>
+                <button className="form--btn" onClick={getMemeImage}>Get a new meme image 🖼️</button>
+            </div>
         </main>
     )
 }
