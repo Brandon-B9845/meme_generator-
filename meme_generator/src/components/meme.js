800d6c317f36d6ca01d36f331fa320d8793375b7
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-02-06 19:25:56 +0100

    added the images that are currently random images from a set of raw data in a file in the app will update to an API later

--- meme_generator/src/components/meme.js
+++ meme_generator/src/components/meme.js
@@ -1,13 +1,17 @@
 import memesData from "../memesData"
+import React from "react"
 
 export default function Meme() {
+    const [image, setImage] = React.useState('')
+
+    
     function getMemeImage(){
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
         const url = memesArray[randomNumber].url
         console.log(url)
+        setImage(url)
     }
-    
     
     return (
         <main className="main">
@@ -15,6 +19,7 @@ export default function Meme() {
                 <input type="text" className="form--input" placeholder="Top text"/>
                 <input type="text" className="form--input" placeholder="Bottom text" />
                 <button className="form--btn" onClick={getMemeImage}>Get a new meme image 🖼️</button>
+                <img src={image} alt=""/>
             </div>
         </main>
     )
