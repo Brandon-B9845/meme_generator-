c8ecb21c026a8cfc7657618fb6271d087d104f54
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-02-15 18:55:10 +0100

    added in a state to display the forms over the image that is rendered

--- meme_generator/src/components/meme.js
+++ meme_generator/src/components/meme.js
@@ -9,6 +9,13 @@ export default function Meme() {
     })
     const [allMemeImages, setAllMemeImages] = React.useState(memesData)
     
+     function handleChange(event){
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+     }
 
     function getMemeImage(){
         const memesArray = allMemeImages.data.memes
@@ -23,11 +30,15 @@ export default function Meme() {
     return (
         <main className="main">
             <div className="form">
-                <input type="text" className="form--input" placeholder="Top text"/>
-                <input type="text" className="form--input" placeholder="Bottom text" />
+                <input type="text" className="form--input" placeholder="Top text" name="topText" value={meme.topText} onChange={handleChange}/>
+                <input type="text" className="form--input" placeholder="Bottom text" name="bottomText" value={meme.bottomText} onChange={handleChange}/>
                 <button className="form--btn" onClick={getMemeImage}>Get a new meme image 🖼️</button>
             </div>
+            <div className="meme">
             <img className="meme--image" src={meme.randomImage} alt=""/>
+            <h2 className="meme--text top">{meme.topText}</h2>
+            <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }
